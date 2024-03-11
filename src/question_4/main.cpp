42c1c257559eb349d5c026067ab96c0971432eea
MichaelGlushko <157078135+MichaelGlushko@users.noreply.github.com>
2024-03-11 03:02:19 +0000

    Finished Q4

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -1,6 +1,32 @@
+#include"question4.h"
+#include<iostream>
+#include<string>
+
+using std::cin;
+using std::cout;
+using std::string;
 
 int main()
 {
+    char choice;
+    double gpa;
+    do {
+    cout<<"\nEnter a double value within the range of 0 to 4: ";
+    cin>>gpa;
+
+    if(gpa>=0 && gpa <= 4) {
+        string letter_grade = gpa_to_letter_grade(gpa);
+        cout<<"\nYour letter grade is: "<<letter_grade;
+    }
+    else {
+        std::cout<<"\nNot within range 0 to 4";
+    }
+    cout<<"\nDo you want to continue? Y or N: ";
+    cin>>choice;
+    
+    }while(choice == 'Y');
+
+    cout<<"\nExited Program\n";
 
     return 0;
 }
--- src/question_4/question4.cpp
+++ src/question_4/question4.cpp
@@ -1,5 +1,28 @@
-//
+#include"question4.h"
+
 bool test_config()
 {
     return true;
+}
+
+std::string gpa_to_letter_grade(double gpa)
+{
+    if(gpa <= 4 && gpa >= 3.5) {
+        return "A";
+    }
+    else if(gpa >= 3) {
+        return "B";
+    }
+    else if(gpa >= 2) {
+        return "C";
+    }
+    else if(gpa >= 1) {
+        return "D";
+    }
+    else if(gpa >= 0) {
+        return "F";
+    }
+    else {
+        return "Not Within range";
+    }
 }
