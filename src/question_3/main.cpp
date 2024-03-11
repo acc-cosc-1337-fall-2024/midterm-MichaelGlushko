efee116fef7eeb049953b3b01af4b13270fdb189
MichaelGlushko <157078135+MichaelGlushko@users.noreply.github.com>
2024-03-11 02:51:50 +0000

    Finished Q3

--- src/question_3/main.cpp
+++ src/question_3/main.cpp
@@ -8,14 +8,18 @@ using std::cout;
 int main()
 {
     char choice;
-    
+
+    do {
     cout<<"\nWould you like to roll the die? Please enter 'Y' for yes, or any other key for no: ";
     cin>>choice;
+    
     if(choice == 'Y') {
         cout<<roll_die()<<"\n";
-        main();
-    }
-    else{
+    } 
+    else {
         cout<<"Exited Program\n";
     }
+    }while(choice == 'Y');
+
+    return 0;
 }
